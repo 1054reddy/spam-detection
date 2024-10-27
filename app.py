85e85704239d2f23cfba8759effab28a8dea21e2
1054reddy <reddyraja1054@gmail.com>
2024-10-27 13:21:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def check_spam():
     return jsonify({'result': result})  # Return the result as JSON
 
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use the port from the environment or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
